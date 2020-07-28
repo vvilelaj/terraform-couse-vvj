5a25de32cdc816e27840b51b471f7f228be97d3f
Victor Vilela <vilela_victor@hotmail.com>
2020-07-28 17:53:31 -0500

    add demo-1

---
+++ demo-1/vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+    default = "us-west-1"
+}
+variable AMI {
+    type = map
+    default = {
+        "us-west-1" = "ami-01b4538323eb79a76"
+        "us_west-2" = "ami-06b94666"
+        "eu-west-1" = "ami-0d729a60"
+    }
+}
