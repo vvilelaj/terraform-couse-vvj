0e8453fde32e818e952a2e5bd3cc3214d30a1cab
Victor Vilela <vilela_victor@hotmail.com>
2020-08-02 20:21:38 -0500

    adds demo-7 : an example of configuring vpc - adds a vpc - adds 3 public subnets - adds 3 private subnets - adds a elastic ip - adds a internet gateway - adds a nat gateway - adds route tables and route table associations

---
+++ demo-7/nat.tf
@@ -0,0 +1,40 @@
+# static public ip
+resource "aws_eip" "nat" {
+  vpc      = true
+}
+
+# nat gw
+resource "aws_nat_gateway" "nat-gw" {
+    allocation_id = aws_eip.nat.id
+    subnet_id = aws_subnet.main-public-1.id
+    depends_on = [
+        aws_internet_gateway.main-gw
+    ]
+}
+
+# vpn setup  for nat
+resource "aws_route_table" "main-private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat-gw.id
+  }
+
+  tags = {
+    Name = "main-private-1"
+  }
+}
+
+# route associations public
+resource "aws_route_table_association" "main-private-1" {
+  subnet_id = aws_subnet.main-private-1.id 
+  route_table_id = aws_route_table.main-private.id
+}
+resource "aws_route_table_association" "main-private-2" {
+  subnet_id = aws_subnet.main-private-2.id
+  route_table_id = aws_route_table.main-private.id
+}
+resource "aws_route_table_association" "main-private-3" {
+  subnet_id = aws_subnet.main-private-3.id
+  route_table_id = aws_route_table.main-private.id
+}
---
+++ demo-7/readme.md
@@ -0,0 +1,21 @@
+# demo-7
+An example of creating a **VPC** with *public* and *private* subnets.
+
+## Files
+|Filename|Description|
+|---|---|
+|provider.tf|All configurations regarding the aws provider.|
+|vars.tf|All variables need to configure the provider and resources.|
+|vpc.tf|All configurations regarding the vpc configuration, private and public subnets.|
+|nat.tf|All configurations regarding the nat configuration and the private subnets.|
+
+## Commands
+```
+terraform init
+```
+```
+terraform init
+```
+```
+terraform init
+```
