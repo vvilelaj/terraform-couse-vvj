6802f13bc5c8ccd555b6486143bc10f37c1f1003
Victor Vilela <vilela_victor@hotmail.com>
2020-08-09 21:46:53 -0500

    adds dem0-15 : creates an ec2 instance within an autoscaling group

---
+++ demo-15/autoscaling-policy.tf
@@ -0,0 +1,57 @@
+# Scale up alarm
+resource "aws_autoscaling_policy" "example-cpu-policy-scaleup" {
+  name                   = "example-cpu-policy-scaleup"
+  autoscaling_group_name = aws_autoscaling_group.example-autoscaling.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "1"
+  cooldown               = "300"
+  policy_type            = "SimpleScaling"
+}
+
+resource "aws_cloudwatch_metric_alarm" "example_cpu_alarm_scaleup" {
+  alarm_name          = "example_cpu_alarm_scaleup"
+  alarm_description   = "example_cpu_alarm_scaleup"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "30"
+
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.example-autoscaling.name
+  }
+
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.example-cpu-policy-scaleup.arn]
+}
+
+# Scale down alarm
+resource "aws_autoscaling_policy" "example-cpu-policy-scaledown" {
+  name                   = "example-cpu-policy-scaledown"
+  autoscaling_group_name = aws_autoscaling_group.example-autoscaling.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = "-1"
+  cooldown               = "300"
+  policy_type            = "SimpleScaling"
+}
+
+resource "aws_cloudwatch_metric_alarm" "example_cpu_alarm_scaledown" {
+  alarm_name          = "example_cpu_alarm_scaledown"
+  alarm_description   = "example_cpu_alarm_scaledown"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "5"
+
+  dimensions = {
+    "AutoScalingGroupName" = aws_autoscaling_group.example-autoscaling.name
+  }
+
+  actions_enabled = true
+  alarm_actions   = [aws_autoscaling_policy.example-cpu-policy-scaledown.arn]
+}
